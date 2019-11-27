ad5aab0e7749ec3849abc72b75f224b2a2db1815
Nishionyama <39794586+Nishinoyama@users.noreply.github.com>
2019-11-27 15:04:51 +0900

    もうむりだ

--- MyFirstIDEA/src/HeavyThread.java
+++ MyFirstIDEA/src/HeavyThread.java
@@ -6,11 +6,5 @@ public class HeavyThread extends Thread{
         this.interval = time;
     }
     public void run(){
-        for(int i=5; i>0; i--){
-            try {
-                Thread.sleep(interval); //処理を(約)1000ms止める．例外の可能性あり
-            } catch (InterruptedException e) { /*略*/ }
-            System.out.println(name + ":" + i);
-        }
     }
 }
--- MyFirstIDEA/src/RacingDog.java
+++ MyFirstIDEA/src/RacingDog.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class RacingDog {
 
@@ -5,8 +6,11 @@ public class RacingDog {
     private double step;
     private int interval;
 
+    private double position;
     private int check;
     private int goal;
+
+    private int zekkocho;
 
     public RacingDog( String name, double step, int interval ){
         this.name = name;
@@ -16,6 +20,32 @@ public class RacingDog {
     }
 
     public void start(){
+        while(true){
+            try {
+                Thread.sleep(interval); //処理を(約)1000ms止める．例外の可能性あり
+                run();
+            } catch (InterruptedException e) { /*略*/ }
+            System.out.println(name + ":" + i);
+        }
+    }
+
+    public void run(){
+        double step = this.step;
+        if( zekkocho > 0 ) {
+            step *= 2;
+            zekkocho--;
+            if( zekkocho == 0 ) {
+                System.out.println( name + "");
+            }
+        }
+
+        position += step;
+
+        Random rnd = new Random()
+        if( zekkocho == 0 && rnd.nextInt(100)==1 ){
+            zekkocho += 10;
+        }
+
     }
 
 }
